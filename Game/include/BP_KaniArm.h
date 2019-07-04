63a09638edca2b7ae569402a4d18495d48bf0a72
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2019-07-04 16:27:20 +0900

    update

--- Game/include/BP_KaniArm.h
+++ Game/include/BP_KaniArm.h
@@ -9,10 +9,20 @@ public:
 	void InnerStart()override;
 	void Update()override;
 
+	//アクション
+	void Charge();
+	void MachineGun();
+	void Rocket();
+	void Lazer();	
+	void Stab();
+
 private:
 	AnimationClip m_initPose;
+	SkeletonIK::IKSetting* m_ikSetting[2] = {};
+
+	int m_muzzleBoneID[2] = {};
 
 	//エフェクト
-	CBillboard m_muzzleFlash;
+	CBillboard m_muzzleFlash; int m_muzzleCnt = 0;
 };
 
--- Game/src/BP_KaniArm.cpp
+++ Game/src/BP_KaniArm.cpp
@@ -12,21 +12,40 @@ void BP_KaniArm::InnerStart() {
 	m_model->Init(L"Resource/modelData/kaniarm.cmo", &m_initPose, 1);
 
 	//腕のIK設定
-	/*setting = m_model[4].GetSkinModel().GetSkeleton().CreateIK();
-	setting->tipBone = m_model[4].FindBone(L"Bone031");
-	setting->rootBone = m_model[4].FindBone(L"Bone026");
+	m_ikSetting[R] = m_model->GetSkinModel().GetSkeleton().CreateIK();
+	m_ikSetting[R]->tipBone = m_model->FindBone(L"Bone031");
+	m_ikSetting[R]->rootBone = m_model->FindBone(L"Bone026");
+	m_ikSetting[R]->iteration = 1;
+	//m_ikSetting[R]->footIKRayEndOffset = 
 
-	setting = m_model[4].GetSkinModel().GetSkeleton().CreateIK();
-	setting->tipBone = m_model[4].FindBone(L"Bone031(mirrored)");
-	setting->rootBone = m_model[4].FindBone(L"Bone026(mirrored)");*/
+	m_ikSetting[L] = m_model->GetSkinModel().GetSkeleton().CreateIK();
+	m_ikSetting[L]->tipBone = m_model->FindBone(L"Bone031(mirrored)");
+	m_ikSetting[L]->rootBone = m_model->FindBone(L"Bone026(mirrored)");
+	m_ikSetting[L]->iteration = 1;
+
+	//
+	m_muzzleBoneID[R] = m_model->FindBoneID(L"Bone031");
+	m_muzzleBoneID[L] = m_model->FindBoneID(L"Bone031(mirrored)");
 
 	//エフェクト
 	m_muzzleFlash.Init(L"Resource/spriteData/kaniFlare.png", 1, false);
 	m_muzzleFlash.GetModel().InitPostDraw(PostDrawModelRender::enAdd);
 	m_muzzleFlash.GetModel().GetSkinModel().SetCullMode(D3D11_CULL_NONE);
+	m_muzzleFlash.GetModel().SetIsShadowCaster(false);
 }
 
 void BP_KaniArm::Update() {
-	m_muzzleFlash.SetPos(m_ptrCore->GetPos());
-	m_muzzleFlash.SetScaleHoldAspectRatio(100.0f);
+	m_ikSetting[0]->targetPos = CVector3::Zero();
+	m_ikSetting[1]->targetPos = CVector3::Zero();
+
+	m_muzzleCnt++;
+	if (m_muzzleCnt <= 4) {
+		m_muzzleFlash.SetIsDraw(true);
+		m_muzzleFlash.SetPos(m_model->GetBonePos(m_muzzleBoneID[L]));
+		m_muzzleFlash.SetScaleHoldAspectRatio(100.0f);
+	}
+	else {
+		m_muzzleFlash.SetIsDraw(false);
+		if (m_muzzleCnt >= 8) { m_muzzleCnt = 0; }
+	}
 }
